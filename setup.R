8f53bf05cfe87d78f16c06a3fddb4f802554a793
ugmi <ugnemmilasiunaite@gmail.com>
2024-09-22 23:19:36 +0200

    initial commit

---
+++ setup.R
@@ -0,0 +1,7 @@
+# library(RCurl)
+set.seed(990101) #Change 990101 to your date of birth
+link <- "https://raw.githubusercontent.com/mskoldSU/MT5003_HT17/master/Projekt/proj_data.csv"
+data_individ <- read.csv(link)
+idx <- sample(1:nrow(data_individ), 1000)
+data_individ <- data_individ[idx, ]
+save(data_individ, file = "proj_data.Rdata")
